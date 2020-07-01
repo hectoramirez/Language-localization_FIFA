80ae1ad42186f14aff01e11230c103d3a5236bdc
hramirez <vanhramirez@gmail.com>
2020-07-01 20:30:40 +0200

    V1.0

--- Tweets processing and sentiment.py
+++ Tweets processing and sentiment.py
@@ -5,8 +5,12 @@ import country_converter as coco
 from geopy.geocoders import Nominatim
 from tqdm import tqdm
 import spacy
-from googletrans import Translator
+import numpy as np
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+# +++++++++++++++++++++++++
+#         Import          +
+# +++++++++++++++++++++++++
 
 # list all files containing tweets
 files = list(glob.iglob('Twitter/Tweets/*.json'))
@@ -26,7 +30,9 @@ for file in files:
 print('\n There are', len(tweets_data), 'tweets in the dataset. \n')
 
 
-# Processing JSON
+# +++++++++++++++++++++++++
+#    Processing JSON      +
+# +++++++++++++++++++++++++
 
 def flatten_tweets(tweets):
     """ Flattens out tweet dictionaries so relevant JSON is
@@ -37,12 +43,14 @@ def flatten_tweets(tweets):
     # Iterate through each tweet
     for tweet_obj in tweets:
 
+        ''' User info'''
         # Store the user screen name in 'user-screen_name'
         tweet_obj['user-screen_name'] = tweet_obj['user']['screen_name']
 
         # Store the user location
         tweet_obj['user-location'] = tweet_obj['user']['location']
 
+        ''' Text info'''
         # Check if this is a 140+ character tweet
         if 'extended_tweet' in tweet_obj:
             # Store the extended tweet text in 'extended_tweet-full_text'
@@ -81,11 +89,18 @@ def flatten_tweets(tweets):
                 tweet_obj['quoted_status-extended_tweet-full_text'] = \
                     tweet_obj['quoted_status']['extended_tweet']['full_text']
 
+        ''' Place info'''
         if 'place' in tweet_obj:
             # Store the country code in 'place-country_code'
             try:
+                tweet_obj['place-country'] = \
+                    tweet_obj['place']['country']
+
                 tweet_obj['place-country_code'] = \
                     tweet_obj['place']['country_code']
+
+                tweet_obj['location-coordinates'] = \
+                    tweet_obj['place']['bounding_box']['coordinates']
             except:
                 pass
 
@@ -94,9 +109,9 @@ def flatten_tweets(tweets):
     return tweets_list
 
 
-def select_text(tweets_frame):
-    """ Assigns the main text to only one column depending
-        on whether the tweet is a RT/quote or not"""
+def select_text(tweets):
+    ''' Assigns the main text to only one column depending
+        on whether the tweet is a RT/quote or not'''
 
     tweets_list = []
 
@@ -127,20 +142,23 @@ columns_all_text = ['text', 'extended_tweet-full_text', 'retweeted_status-text',
 
 # select text
 tweets = select_text(tweets)
-columns = ['text', 'lang', 'user-location', 'place-country_code']
+columns = ['text', 'lang', 'user-location', 'place-country',
+           'place-country_code', 'location-coordinates', 'user-screen_name']
 
 # Create a DataFrame from `tweets`
 df_tweets = pd.DataFrame(tweets, columns=columns)
 # replaces NaNs by Nones
 df_tweets.where(pd.notnull(df_tweets), None, inplace=True)
 
-# ++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++
-df_tweets_sample = df_tweets.copy()[:]  # ++++
-# ++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++
+print('\n Processing JSON (process 1 of 5) finished! \n')
 
-# Languages
+# ++++++++++++++++++++++++++++++++++++++++++
+df_tweets_sample = df_tweets.copy()[:]  # ++
+# ++++++++++++++++++++++++++++++++++++++++++
+
+# +++++++++++++++++++++++++
+#       Languages         +
+# +++++++++++++++++++++++++
 
 with open('Countries/languages.json', 'r', encoding='utf-8') as json_file:
     languages_dict = json.load(json_file)
@@ -153,7 +171,7 @@ for idx, row in df_tweets_sample.iterrows():
     elif lang == 'in':
         name = languages_dict['id']['name']
         names.append(name)
-    elif lang=='iw':
+    elif lang == 'iw':
         name = languages_dict['he']['name']
         names.append(name)
     else:
@@ -163,12 +181,12 @@ for idx, row in df_tweets_sample.iterrows():
 df_tweets_sample['language'] = names
 df_tweets_sample.drop(['lang'], axis=1, inplace=True)
 
-print('\n Process 1 of 5 finished! \n')
+print('\n Languages (process 2 of 5) finished! \n')
 
+# +++++++++++++++++++++++++
+#       Locations         +
+# +++++++++++++++++++++++++
 
-# Locations
-
-# place-country_code
 
 # change codes to iso3
 to_iso3_func = lambda x: coco.convert(names=x, to='iso3', not_found=None) \
@@ -177,7 +195,12 @@ to_iso3_func = lambda x: coco.convert(names=x, to='iso3', not_found=None) \
 df_tweets_sample['place-country_code'] = \
     df_tweets_sample['place-country_code'].apply(to_iso3_func)
 
-# user-locations
+# change name to standard name
+to_std_func = lambda x: coco.convert(names=x, to='name_short', not_found=None) \
+    if x is not None else x
+
+df_tweets_sample['place-country'] = \
+                        df_tweets_sample['place-country_code'].apply(to_std_func)
 
 tqdm.pandas()
 
@@ -207,30 +230,43 @@ def geo_locator(user_location):
 
 # apply geo locator to user-location
 loc = df_tweets_sample['user-location'].progress_apply(geo_locator)
-df_tweets_sample['user_location'] = loc
+df_tweets_sample['user-country_code'] = loc
 
 # change codes to iso3
-df_tweets_sample['user_location'] = \
-    df_tweets_sample['user_location'].apply(to_iso3_func)
+df_tweets_sample['user-country_code'] = \
+    df_tweets_sample['user-country_code'].apply(to_iso3_func)
+
+# create user-country column
+df_tweets_sample['user-country'] = \
+    df_tweets_sample['user-country_code'].apply(to_std_func)
 
 # drop old column
 df_tweets_sample.drop(['user-location'], axis=1, inplace=True)
 
-print('\n Process 2 of 5 finished! \n')
-
-codes = []
+countries, codes = [], []
 for idx, row in df_tweets_sample.iterrows():
     if row['place-country_code'] is None:
-        code = row['user_location']
+        country = row['user-country']
+        code = row['user-country_code']
+        countries.append(country)
         codes.append(code)
     else:
+        countries.append(row['place-country'])
         codes.append(row['place-country_code'])
 
-df_tweets_sample['location'] = codes
-df_tweets_sample.drop(columns=['place-country_code', 'user_location'],
+df_tweets_sample['location'] = countries
+df_tweets_sample['location_code'] = codes
+
+# drop old columns
+df_tweets_sample.drop(columns=['place-country', 'place-country_code',
+                               'user-country', 'user-country_code'],
                       inplace=True)
 
-# Sentiment
+print('\n Locations (process 3 of 5) finished! \n')
+
+# +++++++++++++++++++++++++
+#       Text cleaning     +
+# +++++++++++++++++++++++++
 
 nlp = spacy.load('en_core_web_sm')
 
@@ -246,35 +282,85 @@ def cleaner(string):
     return ' '.join(a_lemmas)
 
 
-df_tweets_sample['text-cleaned'] = \
+df_tweets_sample['text_cleaned'] = \
     df_tweets_sample['text'].progress_apply(cleaner)
 
-print('\n Process 3 of 5 finished! \n')
+# select only not-null tweets not in English
+mask1 = df_tweets_sample['text'].notnull()
+mask2 = df_tweets_sample['language'] != 'English'
+df_masked = df_tweets_sample[mask1 & mask2]
 
-translator = Translator()
+# split dataframe in x equal-size pieces
+df_tweets_sample_splitted = np.array_split(df_masked, 150)
 
-trans = df_tweets_sample['text-cleaned'].progress_apply(
-    translator.translate, dest='en')
-df_tweets_sample['text_english'] = trans.apply(lambda x: x.text)
 
-print('\n Process 4 of 5 finished! \n')
+def tweet_translation(df, idx):
+    """ Translate tweets using googletrans """
+
+    from googletrans import Translator
+
+    translator = Translator()
+
+    try:
+        # translate raw tweet
+        trans = df['text'].apply(translator.translate, dest='en')
+        # create column extracting the translated text
+        df['text_english'] = trans.apply(lambda x: x.text)
+        # append to empty list
+        translations.append(df)
+        # save data in case error happens
+        df.to_csv('Twitter/Translations/translation_{}.csv'.format(idx))
+
+    except Exception as e:
+        print(e, ' -- at index ', idx)
+
+
+translations = []
+for idx, df in enumerate(tqdm(df_tweets_sample_splitted)):
+    tqdm._instances.clear()
+    tweet_translation(df, idx)
+
+df_translations = pd.concat(translations)
+df_english = df_tweets_sample.join(df_translations['text_english'])
+
+# replaces NaNs by Nones
+df_english.where(pd.notnull(df_english), None, inplace=True)
+
+# add original English tweets to text_english by replacing Nones
+texts = []
+for idx, row in df_english.iterrows():
+    if row['text_english'] is None:
+        text = row['text']
+        texts.append(text)
+    else:
+        texts.append(row['text_english'])
+
+df_english['text_english'] = texts
+
+print('\n Text cleaning (process 4 of 5) finished! \n')
+
+
+# +++++++++++++++++++++++++
+#       Sentiment         +
+# +++++++++++++++++++++++++
+
+df_sentiment = df_english.copy()
 
 # instantiate new SentimentIntensityAnalyzer
 sid = SentimentIntensityAnalyzer()
 
-sentiment_scores = df_tweets_sample['text_english'].progress_apply(
-    sid.polarity_scores)
+sentiment_scores = df_sentiment['text_english'].progress_apply(
+                                                            sid.polarity_scores)
 sentiment = sentiment_scores.apply(lambda x: x['compound'])
-df_tweets_sample['sentiment'] = sentiment
+df_sentiment['sentiment'] = sentiment
 
-df_tweets_sample.drop(columns=['text-cleaned'], axis=1, inplace=True)
+cols_order = ['text', 'language', 'location', 'location_code',
+              'location-coordinates', 'sentiment', 'text_english',
+              'text_cleaned', 'user-screen_name']
+df_final = df_sentiment[cols_order]
 
-print('\n Process 5 of 5 finished! \n')
+df_final.to_csv('Twitter/Tweets_sentiment.csv')
 
-#
-cols_order = ['text', 'text_english', 'sentiment', 'language', 'location']
-df_tweets_sample = df_tweets_sample[cols_order]
+print('\n Sentiment (process 5 of 5) finished! \n')
 
-df_tweets_sample.to_csv('Twitter/Tweets_cleaned_full.csv')
-
-print('\n Process completely finished!')
+print(df_final.head())
